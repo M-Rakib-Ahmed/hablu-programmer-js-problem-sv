3bdda183a8e8b18ef9cc3cd58c6c23e66c1b11d5
M Rakib Ahmed <ahmedmrakib24@gmail.com>
2024-12-02 11:15:52 +0600

    first file aded

---
+++ index.js
@@ -0,0 +1,99 @@
+// 1 numbre problem 
+let carName = "Volvo"
+// 1 numbre problem 
+
+
+// 2 number problme
+let firstName = "John";
+let lastName = "Doe";
+let age = 35;
+
+// 2 number problme
+
+
+//  3 number problem
+x = 10
+y = 5
+x  *=y 
+
+//  3 number problem
+
+// 4 number problem
+let lenght = 16; // date type number
+let lastNames = "Johnson" // data type string
+// 4 number problem
+
+const x = {
+    firstName: "John",
+    secondName: "done"
+}// data type object
+
+
+// 5 number problem
+function myFunction(){
+    alert('Hello Wrold');
+}
+// 5 number problem
+
+myFunction()
+
+//  6 number problem
+const person = {
+    name: "Jhone",
+    age: 50,
+}
+
+const nameAndAge = person.name +" is " + person.age;
+alert(nameAndAge)
+
+
+//  6 number problem
+
+// 7 number problem 
+<button onclick="alert('click me')">Click</button>
+
+// 7 number problem 
+
+
+// 8 number problme
+const cars = ["Volvo", "Jeep", "Merceds"];
+alert(cars.length)
+
+const barnd = ["Volvo", "jeep", "Mercedes"]
+barnd[0] = "food"
+// 8 number problme
+
+// 9 number problem 
+let random = Math.random()
+console.log(random);
+
+let largesNameber = Math.random(10, 20)
+console.log(largesNameber);
+
+let sqrts = Math.sqrt(9)
+console.log(sqrts);
+
+
+// 9 number problem 
+
+
+// 10 number problem 
+
+x = 10;
+y = 5;
+alert(x > y);
+
+// 10 number problem 
+
+//  file  2 number problem solve
+
+// 1 number problem 
+function celsiusToFahrehnheit(celsius){
+    return(celsius * 1.8) + 32;
+    
+    
+}
+console.log(celsiusToFahrehnheit(0));
+console.log(celsiusToFahrehnheit(25));
+
+// 1 number problem 
